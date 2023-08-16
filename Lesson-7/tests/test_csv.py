dc2dfdf517bd57dfd5bfb85c8f466acb2d363d25
Vladislav Mironov <vladislav.mironov9524@gmail.com>
2023-08-16 22:33:18 +0300

    test_csv done

--- Lesson-7/tests/test_csv.py
+++ Lesson-7/tests/test_csv.py
@@ -1,14 +1,39 @@
 import csv
+import os
+from os.path import join, dirname, exists, getsize
+
+import pytest
+
+file_name = 'eggs.csv'
+file = join(dirname(__file__), file_name)
+
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-def test_csv():
-    with open('resources/eggs.csv', 'x') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',')
+@pytest.fixture(autouse=True)
+def create_csv():
+    # Записываем данные в файл
+    with open(file, 'w', newline='') as csv_file:
+        csvwriter = csv.writer(csv_file, delimiter=',')
         csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
         csvwriter.writerow(['Alex', 'Serj', 'Yana'])
 
-    with open('resources/eggs.csv') as csvfile:
-        csvreader = csv.reader(csvfile)
+    yield
+
+    os.remove(file)
+
+
+def test_check_csv():
+    # Проверяем записанный файл
+    assert exists(file_name)
+
+
+def test_read_csv():
+    # Считываем данные из файла и проверяем их
+    with open(file) as csv_file:
+        csvreader = csv.reader(csv_file)
         for row in csvreader:
             print(row)
+            assert len(row) == 3
+            assert isinstance(row, list)
 
+        assert getsize(file) == 34
