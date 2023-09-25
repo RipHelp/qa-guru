4274cdeadf70b43fac0b2fa9e08086b1814b1836
Vladislav Mironov <vladislav.mironov9524@gmail.com>
2023-09-25 22:09:11 +0300

    lesson 8

--- Lesson-8/homework/models.py
+++ Lesson-8/homework/models.py
@@ -15,7 +15,7 @@ class Product:
 
     def check_quantity(self, quantity) -> bool or ValueError:
         """
-        Верните True если количество продукта больше или равно запрашиваемому
+            Верните True если количество продукта больше или равно запрашиваемому
             и False в обратном случае
         """
         if self.quantity < 0:
@@ -24,7 +24,7 @@ class Product:
 
     def buy(self, quantity):
         """
-        TODO реализуйте метод покупки
+            реализуйте метод покупки
             Проверьте количество продукта используя метод check_quantity
             Если продуктов не хватает, то выбросите исключение ValueError
         """
@@ -56,12 +56,11 @@ class Cart:
         Если продукт уже есть в корзине, то увеличиваем количество
         """
         if buy_count < 0:
-            raise NotImplementedError
+            raise ValueError
         if product in self.products:
-            self.products[product] += 1
+            self.products[product] += buy_count
         else:
             self.products[product] = buy_count
-
 
     def remove_product(self, product: Product, remove_count=None):
         """
@@ -69,13 +68,25 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
-        raise NotImplementedError
+        if product in self.products.keys():
+            if remove_count is None or remove_count >= self.products[product]:
+                del self.products[product]
+            elif remove_count < self.products[product]:
+                self.products[product] -= remove_count
+        else:
+            raise NotImplementedError(f"Товар {product} отсутвует в корзине!")
 
     def clear(self):
-        raise NotImplementedError
+        """
+        Очистка корзины
+        """
+        self.products.clear()
 
     def get_total_price(self) -> float:
-        raise NotImplementedError
+        total_res = 0
+        for product, quantity in self.products.items():
+            total_res += product.price * quantity
+        return total_res
 
     def buy(self):
         """
@@ -83,4 +94,10 @@ class Cart:
         Учтите, что товаров может не хватать на складе.
         В этом случае нужно выбросить исключение ValueError
         """
-        raise NotImplementedError
+        for product in self.products:
+            try:
+                product.buy(quantity=self.products[product])
+            except ValueError as err:
+                raise err
+            else:
+                self.clear()
